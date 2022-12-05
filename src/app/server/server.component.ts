cbdf064e6142e0f5645de562cd213678dc1cd4ab
Kurt <k3030789@gmail.com>
2022-12-06 00:07:48 +0800

    ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,14 +2,28 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 
 export class ServerComponent {
   serverId: number = 10; // string binding，最後一定會被轉成或是 return string
   serverStatus: string = "offline";
 
+  // constructor 會在 class 成立時執行
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
+
   getServerStatus() {
     return this.serverStatus
+  }
+
+  getColor(){
+    return this.serverStatus === "online" ? "green": "red";
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   allowNweServer = false;
   serverCreationStatus = "No Server was created";
   serverName= "TestServer";
+  serverCreated= false;
   constructor() {
     setTimeout(()=>{
       this.allowNweServer = true;
@@ -22,7 +23,8 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
   onCreateServer(){
-      this.serverCreationStatus = "Server was created!";
+    this.serverCreated = true;
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
